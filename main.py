3de6537ed23bd1102a61fb2e15c2a3b516719256
Vasily3107 <vasilynelis@gmail.com>
2025-03-19 12:01:55 +0200

    GUI update | v1.0.0
     Full functionality of available calculations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.ttk import Combobox
 
+from class_calculator import Car, CarCalculator
+
 root = Tk()
 
 # - - - VOLUME - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -50,11 +52,51 @@ stake_entry.grid(row=2, column=1)
 
 
 # - - - RESULT CALCULATION - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-result_label = Label(root, text='Result:')
-result_button = Button(root, text='Calculate', command=...)
+error_label = Label(root, text='', fg='red', wraplength=100)
+error_label.grid(row=3, column=1)
 
-result_button.grid(row=3, column=0)
-result_label.grid(row=3, column=1)
+def clear_error():
+    error_label.config(text='')
+def show_error(err_msg: str):
+    error_label.config(text=f'Error: {err_msg}')
+
+def calculate():
+    clear_error()
+
+    fueltype = fueltype_dropbox.get().lower()
+
+    try: volume = int(volume_entry.get())
+    except:
+        show_error('volume value must be an integer')
+        return
+
+    try: year = int(year_entry.get())
+    except:
+        show_error('year value must be an integer')
+        return
+
+    country = country_dropbox.get()
+
+    try: stake = float(stake_entry.get())
+    except:
+        show_error('auction stake value must be a real number')
+        return
+
+    tmp_car = Car(fueltype, volume, year, country, stake, True)
+
+    res = CarCalculator(tmp_car)
+
+    result_label.config(text='\n'.join(['Results:',
+                                         f'Import duty: {res.import_duty}',
+                                          f'Excise tax: {res.excise_tax}',
+                                                 f'Vat: {res.vat}',
+                                        f'Pension fund: {res.pension_fund}']))
+
+result_label = Label(root, text='Result:')
+result_label.grid(row=3, column=0)
+
+result_button = Button(root, text='Calculate', command=calculate)
+result_button.grid(row=2, column=2, columnspan=2)
 
 
 # - - - VISUAL SETTINGS - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -62,21 +104,22 @@ root.title('CustomsClearance')
 root.geometry('550x200')
 
 ROW_GAP = 30
-for row_index in range(10):
-    root.grid_rowconfigure(row_index, minsize=ROW_GAP)
+for index in range(10):
+    root.grid_rowconfigure(index, minsize=ROW_GAP)
 
 LABEL_WIDTH = 15
 USER_INPUT_WIDGET_WIDTH = 20
 
 for widget in root.winfo_children():
         
-        if isinstance(widget, (Label, Button)):
+        if type(widget) in (Label, Button):
+            if widget in [result_label, error_label]: continue
             widget.config(width=LABEL_WIDTH)
 
-        if isinstance(widget, Entry):
+        elif type(widget) is Entry:
             widget.config(width=USER_INPUT_WIDGET_WIDTH + 3)
 
-        if isinstance(widget, Combobox):
+        elif type(widget) is Combobox:
             widget.config(width=USER_INPUT_WIDGET_WIDTH)
 
 root.mainloop()
